e4a70922910b4f8db05b7b0cc336ec7bbdda3e3e
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2021-07-28 21:25:41 +0200

    Initial commit
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

---
+++ src/Plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boesing\PsalmPluginStringf;
+
+use Psalm\Plugin\PluginEntryPointInterface;
+use Psalm\Plugin\RegistrationInterface;
+use SimpleXMLElement;
+
+final class Plugin implements PluginEntryPointInterface
+{
+    public function __invoke(RegistrationInterface $registration, ?SimpleXMLElement $config = null): void
+    {
+    }
+}
